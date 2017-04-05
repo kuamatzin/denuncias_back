6798f7a22f11571a926cc0be98ba34e71e7e90b7
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-04-05 14:55:39 -0500

    Mail test

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('denuncias/image', 'DenunciaController@image');
+Route::post('denuncias/image', 'DenunciaController@image');
 Route::resource('denuncias', 'DenunciaController');
