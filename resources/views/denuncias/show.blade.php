43f43fd6b29a5319187cc93634b184e045ad66c6
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-04-11 21:34:54 -0500

    Video player

--- resources/views/denuncias/show.blade.php
+++ resources/views/denuncias/show.blade.php
@@ -1,11 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-        <ul>
-        @foreach($denuncia->imagenes as $imagen)
-            <li>
-                <img src="/storage/{{$imagen}}" width="300px" alt="">
-            </li>
-        @endforeach
-        </ul>
+        <div class="container">
+                <div class="row">
+                        <div class="col-md-6">
+                                <h3>Imágenes de evidencia</h3>
+                                <ul>
+                                        @foreach($denuncia->imagenes as $imagen)
+                                        <li>
+                                                <img src="/storage/{{$imagen}}" width="300px" alt="">
+                                        </li>
+                                        @endforeach
+                                </ul>
+                        </div>
+                        <div class="col-md-6">
+                                <h3>Videos de evidencia</h3>
+                                <ul>
+                                        @foreach($denuncia->videos as $video)
+                                        <li>
+                                                <video width="400" controls>
+                                                        <source src="/storage/{{$video}}" type="video/mp4">
+                                                </video>
+                                        </li>
+                                        @endforeach
+                                </ul>
+                        </div>
+                </div>
+        </div>
 @endsection
