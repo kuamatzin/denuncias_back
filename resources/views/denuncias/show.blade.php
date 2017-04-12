c0c60ae5c79191a3a804799d493f25c2d83cdb9a
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-04-11 20:51:54 -0500

    Panel administración imagenes 4

--- resources/views/denuncias/show.blade.php
+++ resources/views/denuncias/show.blade.php
@@ -3,11 +3,11 @@
 @section('content')
         <ul>
         <li>
-                <img src="storage/{{$denuncia->imagenes[0]}}" width="100px" alt="">
+                <img src="/storage/{{$denuncia->imagenes[0]}}" width="100px" alt="">
         </li>
         @foreach($denuncia->imagenes as $imagen)
             <li>
-                <img src="storage/{{$imagen}}" width="100px" alt="">
+                <img src="/storage/{{$imagen}}" width="100px" alt="">
             </li>
         @endforeach
         </ul>
