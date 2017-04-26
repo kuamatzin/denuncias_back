c80d42c4cd5a7c92715b555ae2fdd6dc917faa98
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-04-26 12:09:55 -0500

    debug fecha

--- app/Http/Controllers/DenunciaController.php
+++ app/Http/Controllers/DenunciaController.php
@@ -110,6 +110,7 @@ class DenunciaController extends Controller
         $videos = $denuncia->videos != null ? $denuncia->videos : [];
 
         if($request->denuncia_id == 'new') {
+            dd($request->fecha);
             $denuncia->anonima = $request->denunciaAnonima == null ? '': $request->denunciaAnonima;
             $denuncia->nombre_denuncia = $request->nombre_denuncia;
             $denuncia->descripcion = $request->descripcion;
